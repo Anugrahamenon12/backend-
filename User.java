90de1317fb509da72907b99f82f8c90c8b76f467
Anugrahamenon12 <anugrahamenon20@gmail.com>
2026-04-03 15:27:47 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,16 @@
+package com.carbon.app.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    // getters & setters
+}
